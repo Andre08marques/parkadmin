ff4d264dd8481efbb422f6c2194cd03794b4a927
Luiz <luizvitoralves@gmail.com>
2025-09-04 10:59:28 -0300

    add view contratos + templates

--- main/apps/customers/views.py
+++ main/apps/customers/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from .models import Contrato
+from .forms import ContratoAddForm
 
-# Create your views here.
+
+class ListContrato(View):
+
+    def get(self, request):
+
+        contratos = Contrato.objects.all()
+        context = {
+            'contratos': contratos
+        }
+        return render(request, 'list_contrato.html', context)
