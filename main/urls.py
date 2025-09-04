97fc0d292b91b2baadc055c3d7d6949537b8ada9
André Marques <andre.ti.marques@gmail.com>
2025-09-04 07:38:21 -0300

    ajuste customersa url

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('main.apps.home.urls')),
-    path('core/', include('main.apps.core.urls'))
+    path('core/', include('main.apps.core.urls')),
+    path('customers/', include('main.apps.customers.urls'))
+    
 ]
