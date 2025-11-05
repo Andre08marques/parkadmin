522b3c4c9ea4034cda802d6d38d12aed8aae1415
andremarquesdevops <andremarquesdevops@MacBook-Air-de-andremarquesdevops.local>
2025-11-05 17:36:26 -0300

    feature/parking #batch

--- main/settings.py
+++ main/settings.py
@@ -110,11 +110,14 @@ USE_I18N = True
 TIME_ZONE = 'America/Sao_Paulo'
 USE_TZ = True
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # LOGIN
 LOGOUT_REDIRECT_URL = '/accounts/login/'
